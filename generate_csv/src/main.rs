1fd711d6e762b88a1b1617170531feda3472b838
Tomaz Canabrava <tcanabrava@kde.org>
2026-04-21 20:06:39 +0200

    Finish generating random csv

--- generate_csv/src/main.rs
+++ generate_csv/src/main.rs
@@ -1,5 +1,3 @@
-use core::num;
-
 use chrono::{DateTime, Utc};
 use rand::prelude::*;
 use serde::{Deserialize, Serialize};
@@ -244,7 +242,7 @@ fn generate_travel_journal(
     let mut ret: Vec<TravelJournal> = Vec::<TravelJournal>::new();
     for p in 0..num_participants {
         // 50 log entries per person
-        for i in 0..50 {
+        for _ in 0..50 {
             let time = Utc::now();
             let t = TravelJournal {
                 participant_id: p,
@@ -260,19 +258,53 @@ fn generate_travel_journal(
     ret
 }
 
+fn save_csv<T>(filename: &str, data: &Vec<T>)
+where T: serde::Serialize {
+    let mut wtr = csv::Writer::from_path(filename)
+        .expect("Could not open file");
+
+    for d in data {
+        wtr.serialize(d)
+            .expect("Could not serialize data");
+    }
+    wtr.flush()
+        .expect("Could not write file in disk")
+}
+
 fn main() {
     let mut rng = rand::rng();
 
     // Generate 100 participants:
     let participants: Vec<Participant> = generate_participants(100, &mut rng);
+    save_csv("participants.csv", &participants);
+
     let (buildings, apts) = generate_buildings(50, &mut rng);
+    save_csv("buildings.csv", &buildings);
+    save_csv("apartments.csv", &apts);
+
     let employers = generate_employer(15, buildings.len() as i32, &mut rng);
+    save_csv("employers.csv", &employers);
 
     // Create less jobs than population so some people don't have jobs.
     let jobs = generate_jobs(90, employers.len() as i32, &mut rng);
+    save_csv("jobs.csv", &jobs);
+
     let travel_journal =
         generate_travel_journal(participants.len() as i32, buildings.len() as i32, &mut rng);
+    save_csv("traveljournal.csv", &travel_journal);
+    
     let financial_journal = FinancialJournal::generate_random(participants.len() as i32, &mut rng);
-    let participant_status =
-        ParticipantStatusLog::generate_random(participants.len() as i32, &mut rng);
+    save_csv("financialjournal.csv", &financial_journal);
+    
+    for i in 0..10 {
+        let participant_status =
+            ParticipantStatusLog::generate_random(participants.len() as i32, &mut rng);
+        let filename = format!("participantStatusLog{}", i);
+        save_csv(&filename, &participant_status);
+    }
+
+    // I Might have not undertstood the `N` in the ParticipantStatusLog<N>.
+    // is the N for one participant? if so, I don't see the need to store it's id.
+    // Is it for Weekly data? A bit confusing.
+
 }
