854f008e875f2935dbf1513667de5bdd210fce23
Tomaz Canabrava <tcanabrava@kde.org>
2026-04-25 11:50:02 +0200

    Start the UI for the interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,34 @@
-import './App.css'
-
 import Box from '@mui/material/Box';
 import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
+import CssBaseline from '@mui/material/CssBaseline';
 
 import * as React from 'react';
+import Home from './Home'
+import Graph1 from './Graph'
 
 function App() {
-  const [tabIndex, setTabIndex] = React.useState(0)
+  const [tabIndex, setTabIndex] = React.useState("0")
   const handleChange = (event, newValue ) => {
     setTabIndex(newValue);
   };
 
   return (
     <>
+      <CssBaseline />
       <TabContext value={tabIndex}>
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <TabList onChange={handleChange} aria-label="lab API tabs example">
-            <Tab label="Item One" value="1" />
-            <Tab label="Item Two" value="2" />
-            <Tab label="Item Three" value="3" />
+            <Tab label="Home" value="0" />
+            <Tab label="Graph1" value="1" />
+            <Tab label="Graph2" value="2" />
           </TabList>
         </Box>
-        <TabPanel value="1">Item One</TabPanel>
-        <TabPanel value="2">Item Two</TabPanel>
-        <TabPanel value="3">Item Three</TabPanel>
+        <TabPanel value="0"><Home/></TabPanel>
+        <TabPanel value="1"><Graph1/></TabPanel>
+        <TabPanel value="2">Item Three</TabPanel>
       </TabContext>
     </>
   )
---
+++ src/Home.jsx
@@ -0,0 +1,7 @@
+function Home() {
+    return (<>
+        Home Called
+    </>)
+}
+
+export default Home
