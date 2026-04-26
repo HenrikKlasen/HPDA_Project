31029b055f5a599d106a2c39794448ced5dbda5a
Tomaz Canabrava <tcanabrava@kde.org>
2026-04-26 12:40:20 +0200

    Return all the datafiles in json format

--- backend/src/bin/csv2parquet.rs
+++ backend/src/bin/csv2parquet.rs
@@ -6,22 +6,26 @@ use std::path::{Path, PathBuf};
 fn polars_parquet_transform(file_path: PathBuf, new_path: PathBuf) {
     let f1 = file_path.to_str().unwrap();
     let f2 = new_path.to_str().unwrap();
-    
+
     let lf = LazyCsvReader::new(PlRefPath::new(f1))
         .with_has_header(true)
         .with_infer_schema_length(Some(1000))
         // A first try failed because a column had a `NA` do denote `no value`. this is not Nashville.
-        .with_null_values(Some(NullValues::AllColumnsSingle(PlSmallStr::from_str("NA"))))
-        .finish().unwrap()
+        .with_null_values(Some(NullValues::AllColumnsSingle(PlSmallStr::from_str(
+            "NA",
+        ))))
+        .finish()
+        .unwrap()
         .with_new_streaming(true);
 
     let sync_type = SinkDestination::File {
-        target: SinkTarget::Path(PlRefPath::new(f2))
+        target: SinkTarget::Path(PlRefPath::new(f2)),
     };
 
-    lf.sink(sync_type,
-            FileWriteFormat::Parquet(Default::default()),
-            UnifiedSinkArgs::default()
+    lf.sink(
+        sync_type,
+        FileWriteFormat::Parquet(Default::default()),
+        UnifiedSinkArgs::default(),
     )
     .unwrap()
     .collect()
@@ -45,9 +49,9 @@ fn read_recurse(p: &std::path::Path) {
         if path.extension() != Some("csv".as_ref()) {
             continue;
         }
-    
+
         let filename = entry.file_name().into_string().unwrap();
-        if ! filename.ends_with("csv") {
+        if !filename.ends_with("csv") {
             continue;
         }
 
@@ -58,7 +62,7 @@ fn read_recurse(p: &std::path::Path) {
 fn main() -> PolarsResult<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        println!("Missing folder for csv files.");        
+        println!("Missing folder for csv files.");
     }
     let csv_folder = args.last().unwrap();
     println!("{:?}", csv_folder);
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,82 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
+
+use axum::{
+    Json, Router,
+    extract::State,
+    http::StatusCode,
+    routing::{get, post},
+};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct DataFile {
+    display_name: String,
+    full_path: String,
+}
+
+fn read_recurse(p: &std::path::Path, res: &mut Vec<DataFile>) {
+    let entries = std::fs::read_dir(p).unwrap();
+    for entry in entries {
+        let entry = entry.unwrap();
+        if entry.metadata().unwrap().is_dir() {
+            read_recurse(&entry.path(), res);
+        }
+
+        let path = entry.path();
+        if path.extension() != Some("parquet".as_ref()) {}
+        let stem = path.file_stem().unwrap();
+
+        res.push(DataFile {
+            display_name: stem.to_string_lossy().into_owned(),
+            full_path: path.to_string_lossy().into_owned(),
+        });
+    }
+}
+
+async fn get_data_files(
+    State(state): State<ServerState>,
+) -> Result<Json<Vec<DataFile>>, StatusCode> {
+    return Ok(Json(state.data_files));
+}
+
+fn get_data_assets_folder() -> PathBuf {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Missing folder for csv files.");
+    }
+    let csv_folder = args.last().unwrap();
+    println!("{:?}", csv_folder);
+
+    let path = PathBuf::from(csv_folder);
+    return path;
+}
+
+#[derive(Clone)]
+struct ServerState {
+    data_assets: PathBuf,
+    data_files: Vec<DataFile>,
+}
+
+#[tokio::main]
+async fn main() {
+    let data_assets = get_data_assets_folder();
+    tracing_subscriber::fmt::init();
+
+    let mut data_files = Vec::<DataFile>::new();
+    read_recurse(&data_assets, &mut data_files);
+
+    let state = ServerState {
+        data_assets: data_assets,
+        data_files: data_files,
+    };
+
+    let app = Router::new()
+        .route("/get_data_files", get(get_data_files))
+        .with_state(state);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
