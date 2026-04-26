00bbf0a7856471d965305671321603742a3b5ffc
Henrik Klasen <henrik.klasen.001@student.uni.lu>
2026-04-26 13:40:45 +0200

    Revert "Map, fails to scroll, but zoom is working"
    This reverts commit 5ea6fc65651d4471ced505d466f534feb0d9fc6d.

--- frontend/src/utils/metrics.js
+++
@@ -1,9 +0,0 @@
-export function formatPercent(value) {
-  return `${Number(value).toFixed(2)}%`;
-}
-
-export function formatDuration(seconds) {
-  const mins = Math.floor(seconds / 60);
-  const secs = seconds % 60;
-  return `${mins}m ${secs}s`;
-}
